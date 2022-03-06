19bedf38dc158edd8b252e79b3ee0e34e6ee78dd
tramontana1969 <snegovskojm@gmail.com>
2022-03-06 16:11:22 +0300

    fix content-single page

--- wp-content/themes/automobile-child/template-parts/post/content-single.php
+++ wp-content/themes/automobile-child/template-parts/post/content-single.php
@@ -15,16 +15,17 @@
                 <h3 class="single-post"><?php the_title();?></h3>
                 <div class="metabox">
                     <span class="entry-date"><i class="fas fa-calendar-alt"></i><?php echo esc_html( get_the_date()); ?></span>
+                    <span class="entry-date"><i></i>Year: <?php echo get_post_meta($post->ID, 'Year', true);; ?></span>
                 </div>
                 <?php if(has_post_thumbnail()) { ?>
                     <?php the_post_thumbnail(); ?>
                 <?php }?>
                 <p><?php the_content(); ?></p>
-                <p>Price: <?php echo get_post_meta($post->ID, 'Price', true);; ?></p>
-                <p>Year: <?php echo get_post_meta($post->ID, 'Year', true);; ?></p>
+                <p style="font-size: 35px; color: black">Price: <?php echo get_post_meta($post->ID, 'Price', true);?> $</p>
                 <p><?php $taxonomy = get_the_taxonomies($post->ID);
                     echo print_r(($taxonomy['marks']), true); ?></p>
                 <p><?php echo print_r($taxonomy['models'], true)?></p>
+                <p><?php echo print_r($taxonomy['fuel_types'], true)?></p>
                 <div class="single-post-tags mt-3"><?php the_tags(); ?></div>
                 <hr>
             </div>
