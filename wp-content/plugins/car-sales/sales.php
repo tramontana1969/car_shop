85d0bfbf9683433c69378d1bec21e5830a520f1c
tramontana1969 <snegovskojm@gmail.com>
2022-03-09 14:14:49 +0300

    combine changing sale status and viewing checkboxes functions

--- wp-content/plugins/car-sales/sales.php
+++ wp-content/plugins/car-sales/sales.php
@@ -14,6 +14,7 @@ function set_default_sale_status() {
     update_post_meta($id, 'sales', 'off');
 }
 
+
 add_action( 'manage_edit-cars_columns', function ( $columns ) {
     $my_columns = [
         'sale' => 'Sale',
@@ -21,9 +22,9 @@ add_action( 'manage_edit-cars_columns', function ( $columns ) {
     return $columns + $my_columns ;
 } );
 
-add_filter( 'manage_cars_posts_custom_column', 'view_all_checkboxes');
 
-function view_all_checkboxes($column_name) {
+add_filter( 'manage_cars_posts_custom_column', 'checkboxes_methods');
+function checkboxes_methods($column_name) {
     $id = get_the_ID();
     $post = acf_get_posts();
     $sale_status_array = array_values(get_post_meta($id,'sales'));
@@ -32,46 +33,34 @@ function view_all_checkboxes($column_name) {
         $sale_status = $sale_status_array[$i];
     }
     if ($column_name == 'sale') {
-        if($post[0] == get_post()) {
-            echo "<form method='post'>";
-            if ( $sale_status == 'off') {
-                echo "<input type='hidden'/>";
-            }
-            elseif ($sale_status == 'on') {
-                echo "<input type='hidden'>";
-            }
-            echo "</form>";
-        }
-        echo "<form method='post'>";
-        the_meta();
-        if ( $sale_status == 'off') {
-            echo "<input id='empty_checkbox_$id' type='checkbox' name='_sales[$id]' value='$id'/>";
-        }
-        elseif ($sale_status == 'on') {
-            echo "<input id='checkbox_$id' type='checkbox' name='_sales[$id]' value='$id' checked/>";
-        }
-        echo "<input onclick='change_status()' type='submit' value='Change status' />";
-        echo "</form>";
-    }
-};
-
-add_action('manage_cars_posts_custom_column', 'change_cars_sale_status', 10, 5);
-function change_cars_sale_status($column_name, $id) {
-
-    $sale_status_array = array_values(get_post_meta($id,'sales'));
-
-    for ($i = 0; $i < count($sale_status_array); $i++) {
-        $sale_status = $sale_status_array[$i];
-    }
-
-    if ($column_name == 'sale') {
         if ($_SERVER['REQUEST_METHOD'] == 'POST' && $sale_status == 'off' && $_POST['_sales'][$id] == $id) {
             update_post_meta($id, 'sales', 'on');
-            wp_get_update_https_url();
+            echo "<p>Added to sales</p>";
         }
-        if ($_SERVER['REQUEST_METHOD'] == 'POST' && $sale_status == 'on' && $_POST['_sales'][$id] == $id) {
+        else if ($_SERVER['REQUEST_METHOD'] == 'POST' && $sale_status == 'on' && $_POST['_sales'][$id] == $id) {
             update_post_meta($id, 'sales', 'off');
-            wp_get_update_https_url();
+            echo "<p>Removed from sales</p>";
+        }
+        else {
+            if($post[0] == get_post()) {
+                echo "<form method='post'>";
+                if ( $sale_status == 'off') {
+                    echo "<input type='hidden'/>";
+                }
+                elseif ($sale_status == 'on') {
+                    echo "<input type='hidden'>";
+                }
+                echo "</form>";
+            }
+            echo "<form method='post'>";
+            if ( $sale_status == 'off') {
+                echo "<input id='empty_checkbox_$id' type='checkbox' name='_sales[$id]' value='$id'/>";
+            }
+            elseif ($sale_status == 'on') {
+                echo "<input id='checked_checkbox_$id' type='checkbox' name='_sales[$id]' value='$id' checked/>";
+            }
+            echo "<input type='submit' value='Change status' />";
+            echo "</form>";
         }
     }
-}
+};
